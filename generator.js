0e8d2d5b2292cb487e4fbb58123496cbdca580c2
HassanHassouna <hhasona@gmail.com>
2023-12-31 12:47:37 +0200

    feat

--- generator.js
+++ generator.js
@@ -27,3 +27,27 @@ const onInsert = function (err, docs) {
 }
 
 Person.collection.insertMany(data, onInsert)
+
+//Find the count of people who make more than 25000.
+
+db.linkedon.count({salary: {$gt: 25000}})
+
+// Find only the top 3 earning people, and return only their first name and salary.
+
+db.linkedon.find({}, {firstName: 1, salary: 1}).sort({salary: -1}).limit(3)
+
+// Find the total number of people currently working at Walmart that are earning at least 7000.
+
+db.linkedon.count({currentCompany: {name: "Walmart"}, salary: {$gte: 7000}})
+
+// Find the current company name, full name, and salary of the highest earning person that currently works in either Sales or Retail.
+// Make sure you’re returning only those fields (company name, first and last name, and salary.)
+// You should find it to be Holly Gonzales, working at Groupon for 20,000.
+
+db.linkedon.find({$or: [{currentCompany: {industry: "Sales"}}, {currentCompany: {industry: "Retail"}}]}, {firstName: 1, lastName: 1, salary: 1, "currentCompany.name": 1}).sort({salary: -1}).limit(1)
+
+// Find the number of people who have ever worked at Apple, past or present.
+
+db.linkedon.count({$or: [{currentCompany: {name: "Apple"}}, {previousCompanies: {name: "Apple"}}]})
+
+
